3a5541cfeff0e099bdcf01ae8d1f9b5c096a1d9f
Тимур <@DESKTOP-LK5N1TP>
2025-11-13 01:59:37 +0300

    Calculator DONE

--- Geometry/Program.cs
+++ Geometry/Program.cs
@@ -20,17 +20,16 @@ namespace Calculator
 		{
 			Console.Write("Введите арифметическое выражение: ");
 			//string expression = "22*33/44/2*8*3";
-			string expression = "22+33-44/2+8*3";
-			//string expression = Console.ReadLine();
-			expression = "22+33-44/2+8*3+1";
+			expression = "5 + (1 + (2 + (22 + 3)*2 + (33 - 44))/(2 + 8)*3 + 1)*2 - 2";
+			//expression = "(4*(2+3) + (3+2)*4)*3";
+			//expression = "(11 + (55+(22+33)/4-5)*2*3+3)*(3+2)";
+			expression = "5 + (1 + (2 + (22 + 3)*2 + (33 + 44))/(2 + 8)*3 + 1)*2 - 2";
+			//expression = "22+33-44/2+8*3+1";
 			//expression = Console.ReadLine();
 			expression = expression.Replace(",", ".");
 			expression = expression.Replace(" ", "");
 			Console.WriteLine(expression);
 
-			char[] operators = new char[] { '+', '-', '*', '/' };
-			string[] operands = expression.Split(operators);
-			double[] values = new double[operands.Length];
 			operands = expression.Split(operators);
 			values = new double[operands.Length];
 			for (int i = 0; i < operands.Length; i++)
@@ -40,60 +39,25 @@ namespace Calculator
 			}
 			Console.WriteLine();
 
-			char[] digits = "0123456789.".ToCharArray();
 			/*for (int i = 0; i < digits.Length; i++)
 			{
 				Console.Write($"{digits[i]}\t");
 			}
 			Console.WriteLine();*/
+			//Console.WriteLine(expression);
 
-			string[] operations = expression.Split(digits);
 			operations = expression.Split(digits);
 			operations = operations.Where(operation => operation != "").ToArray();  //LINQ
-			for (int i = 0; i < operations.Length; i++)
+			/*for (int i = 0; i < operations.Length; i++)
 			{
 				Console.Write($"{operations[i]}\t");
 			}
-			Console.WriteLine();
+			Console.WriteLine();*/
 
-			while (operations[0] != "")
-			{
-				//int i = 0;
-				for (int i = 0; i < operations.Length; i++)
-					for (int i = 0; i < operations.Length; i++)
-					{
-						if (operations[i] == "*" || operations[i] == "/")
-						{
-							if (operations[i] == "*") values[i] *= values[i + 1];
-							if (operations[i] == "/") values[i] /= values[i + 1];
-							for (int index = i; index < operations.Length - 1; index++) operations[index] = operations[index + 1];
-							for (int index = i + 1; index < values.Length - 1; index++) values[index] = values[index + 1];
-							operations[operations.Length - 1] = "";
-							values[values.Length - 1] = 0;
-							Shift(i);
-							if (operations[i] == "*" || operations[i] == "/") i--;
-						}
-						if (operations[i] == "*" || operations[i] == "/") i--;
-					}
-				for (int i = 0; i < operations.Length; i++)
-					for (int i = 0; i < operations.Length; i++)
-					{
-						if (operations[i] == "+" || operations[i] == "-")
-						{
-							if (operations[i] == "+") values[i] += values[i + 1];
-							if (operations[i] == "-") values[i] -= values[i + 1];
-							for (int index = i; index < operations.Length - 1; index++) operations[index] = operations[index + 1];
-							for (int index = i + 1; index < values.Length - 1; index++) values[index] = values[index + 1];
-							operations[operations.Length - 1] = "";
-							values[values.Length - 1] = 0;
-							Shift(i);
-							if (operations[i] == "+" || operations[i] == "-") i--;
-						}
-						if (operations[i] == "+" || operations[i] == "-") i--;
-					}
+			Console.WriteLine(Calculate(expression));
 
-			}
-			Console.WriteLine(values[0]);
+			Console.WriteLine(Explorer(expression));
+			Console.WriteLine(expression);
 
 #if CALC_IF
 			if (expression.Contains("+"))
@@ -118,12 +82,104 @@ namespace Calculator
 #endif
 
 		}
-		static void Shift(object[] arr, int index)
+		static string Explorer(string expression)
+		{
+			for (int i = 0; i < expression.Length; i++)
+			{
+				if (expression[i] == '(')
+				{
+					for (int j = i + 1; j < expression.Length; j++)
+					{
+						if (expression[j] == ')')
+						{
+							string substring = expression.Substring(i + 1, j - i - 1);
+							//if (expression.Count(s => s == '(') != expression.Count(s => s == ')')) return substring;//expression.Substring(0,j);
+							//double local_result = Calculate(substring.Substring(1, substring.Length - 2));
+							//expression = expression.Replace(substring, local_result.ToString());
+							//break;
+							if (!substring.Contains('(') && !substring.Contains(')'))
+							{
+								double result = Calculate(substring);
+								Program.expression = Program.expression.Replace($"({substring})", result.ToString());
+								Explorer(Program.expression);
+							}
+						}
+						if (expression[j] == '(')
+						{
+							//string substring = Explorer(expression.Substring(j));
+							//Program.expression = expression.Replace(substring, Calculate(substring.Substring(1, substring.Length-2)).ToString());
+							string substring = expression.Substring(j + 1, expression.Length - j - 1);
+							Explorer(substring);
+						}
+					}
+				}
+				if (expression[i] == ')')
+				{
+					string substring = expression.Substring(0, i);
+					if (!substring.Contains('(') && !substring.Contains(')'))
+					{
+						double result = Calculate(substring);
+						Program.expression = Program.expression.Replace($"({substring})", result.ToString());
+					}
+					Explorer(Program.expression);
+				}
+			}
+			return Calculate(Program.expression).ToString();
+		}
+		static double Calculate(string expression)
+		{
+			operands = expression.Split(operators);
+			values = new double[operands.Length];
+			for (int i = 0; i < operands.Length; i++)
+			{
+				values[i] = Convert.ToDouble(operands[i]);
+				Console.Write($"{values[i]}\t");
+			}
+			Console.WriteLine();
+
+			/*for (int i = 0; i < digits.Length; i++)
+			{
+				Console.Write($"{digits[i]}\t");
+			}
+			Console.WriteLine();*/
+
+			operations = expression.Split(digits);
+			operations = operations.Where(operation => operation != "").ToArray();  //LINQ
+			/*for (int i = 0; i < operations.Length; i++)
+			{
+				Console.Write($"{operations[i]}\t");
+			}
+			Console.WriteLine();*/
+
+			while (operations[0] != "")
+			{
+				//int i = 0;
+				for (int i = 0; i < operations.Length; i++)
+				{
+					if (operations[i] == "*" || operations[i] == "/")
+					{
+						if (operations[i] == "*") values[i] *= values[i + 1];
+						if (operations[i] == "/") values[i] /= values[i + 1];
+						Shift(i);
+						if (operations[i] == "*" || operations[i] == "/") i--;
+					}
+				}
+				for (int i = 0; i < operations.Length; i++)
+				{
+					if (operations[i] == "+" || operations[i] == "-")
+					{
+						if (operations[i] == "+") values[i] += values[i + 1];
+						if (operations[i] == "-") values[i] -= values[i + 1];
+						Shift(i);
+						if (operations[i] == "+" || operations[i] == "-") i--;
+					}
+				}
+			}
+			//Console.WriteLine(values[0]);
+			return values[0];
+		}
 		static void Shift(int index)
 		{
-			for (int i = index; i < arr.Length; i++)
-				arr[i] = arr[i + 1];
-			arr[arr.Length - 1] = new object();
 			for (int i = index; i < operations.Length - 1; i++) operations[i] = operations[i + 1];
 			for (int i = index + 1; i < values.Length - 1; i++) values[i] = values[i + 1];
 			operations[operations.Length - 1] = "";
