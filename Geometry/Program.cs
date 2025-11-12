15b203cb5da7a7b9345f198b806cb80418e0c657
Тимур <@DESKTOP-LK5N1TP>
2025-11-13 01:52:28 +0300

    Calculator

--- Geometry/Program.cs
+++ Geometry/Program.cs
@@ -10,11 +10,20 @@ namespace Calculator
 {
 	class Program
 	{
+		static string expression = "";
+		static readonly char[] operators = new char[] { '+', '-', '*', '/' };
+		static string[] operands;
+		static double[] values;
+		static readonly char[] digits = "0123456789.".ToCharArray();
+		static string[] operations;
 		static void Main(string[] args)
 		{
 			Console.Write("Введите арифметическое выражение: ");
+			//string expression = "22*33/44/2*8*3";
 			string expression = "22+33-44/2+8*3";
 			//string expression = Console.ReadLine();
+			expression = "22+33-44/2+8*3+1";
+			//expression = Console.ReadLine();
 			expression = expression.Replace(",", ".");
 			expression = expression.Replace(" ", "");
 			Console.WriteLine(expression);
@@ -22,6 +31,8 @@ namespace Calculator
 			char[] operators = new char[] { '+', '-', '*', '/' };
 			string[] operands = expression.Split(operators);
 			double[] values = new double[operands.Length];
+			operands = expression.Split(operators);
+			values = new double[operands.Length];
 			for (int i = 0; i < operands.Length; i++)
 			{
 				values[i] = Convert.ToDouble(operands[i]);
@@ -37,12 +48,52 @@ namespace Calculator
 			Console.WriteLine();*/
 
 			string[] operations = expression.Split(digits);
+			operations = expression.Split(digits);
 			operations = operations.Where(operation => operation != "").ToArray();  //LINQ
 			for (int i = 0; i < operations.Length; i++)
 			{
 				Console.Write($"{operations[i]}\t");
 			}
 			Console.WriteLine();
+
+			while (operations[0] != "")
+			{
+				//int i = 0;
+				for (int i = 0; i < operations.Length; i++)
+					for (int i = 0; i < operations.Length; i++)
+					{
+						if (operations[i] == "*" || operations[i] == "/")
+						{
+							if (operations[i] == "*") values[i] *= values[i + 1];
+							if (operations[i] == "/") values[i] /= values[i + 1];
+							for (int index = i; index < operations.Length - 1; index++) operations[index] = operations[index + 1];
+							for (int index = i + 1; index < values.Length - 1; index++) values[index] = values[index + 1];
+							operations[operations.Length - 1] = "";
+							values[values.Length - 1] = 0;
+							Shift(i);
+							if (operations[i] == "*" || operations[i] == "/") i--;
+						}
+						if (operations[i] == "*" || operations[i] == "/") i--;
+					}
+				for (int i = 0; i < operations.Length; i++)
+					for (int i = 0; i < operations.Length; i++)
+					{
+						if (operations[i] == "+" || operations[i] == "-")
+						{
+							if (operations[i] == "+") values[i] += values[i + 1];
+							if (operations[i] == "-") values[i] -= values[i + 1];
+							for (int index = i; index < operations.Length - 1; index++) operations[index] = operations[index + 1];
+							for (int index = i + 1; index < values.Length - 1; index++) values[index] = values[index + 1];
+							operations[operations.Length - 1] = "";
+							values[values.Length - 1] = 0;
+							Shift(i);
+							if (operations[i] == "+" || operations[i] == "-") i--;
+						}
+						if (operations[i] == "+" || operations[i] == "-") i--;
+					}
+
+			}
+			Console.WriteLine(values[0]);
 
 #if CALC_IF
 			if (expression.Contains("+"))
@@ -66,6 +117,17 @@ namespace Calculator
 			} 
 #endif
 
+		}
+		static void Shift(object[] arr, int index)
+		static void Shift(int index)
+		{
+			for (int i = index; i < arr.Length; i++)
+				arr[i] = arr[i + 1];
+			arr[arr.Length - 1] = new object();
+			for (int i = index; i < operations.Length - 1; i++) operations[i] = operations[i + 1];
+			for (int i = index + 1; i < values.Length - 1; i++) values[i] = values[i + 1];
+			operations[operations.Length - 1] = "";
+			values[values.Length - 1] = 0;
 		}
 	}
 }
